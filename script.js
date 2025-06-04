94245094cea5676ba78b25be0c9de87b70136d7d
kkris26 <krisnuartha09@gmail.com>
2025-06-04 15:14:21 +0800

    add : get weather API

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 function getCurrentLocation() {
-  // const options = {
-  //   enableHighAccuracy: true,
-  //   timeout: 5000,
-  //   maximumAge: 0,
-  // };
-
   function success(pos) {
     const crd = pos.coords;
 
@@ -15,6 +9,7 @@ function getCurrentLocation() {
     console.log(`Longitude: ${crd.longitude}`);
     console.log(`More or less ${crd.accuracy} meters.`);
     getLocationName(lat, lon);
+    getWeather(lat, lon);
   }
 
   function error(err) {
@@ -30,10 +25,18 @@ async function getLocationName(lat, lon) {
   const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&zoom=18&format=jsonv2`;
   try {
     const response = await fetch(url);
-    const data = await response.json()
+    const data = await response.json();
     console.log(response);
     console.log(data);
   } catch (error) {
     console.log("Failed to fetch");
   }
+}
+
+async function getWeather(lat, lon) {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,sunrise,sunset,temperature_2m_max,temperature_2m_min&current=weather_code,is_day,temperature_2m,wind_speed_10m,relative_humidity_2m&timezone=auto`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
 }
