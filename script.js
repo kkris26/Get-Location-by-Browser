5229fcb2cc5e9c4e86a0140dc2373eee1706598d
kkris26 <krisnuartha09@gmail.com>
2025-06-04 17:11:10 +0800

    add : loading first

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function getCurrentLocation() {
     console.log(`Latitude : ${crd.latitude}`);
     console.log(`Longitude: ${crd.longitude}`);
     console.log(`More or less ${crd.accuracy} meters.`);
-    // getLocationName(lat, lon);
-    // getWeather(lat, lon);
+    getLocationName(lat, lon);
+    getWeather(lat, lon);
     displayData(lat, lon);
   }
 
@@ -22,25 +22,25 @@ function getCurrentLocation() {
 
 getCurrentLocation();
 
-// async function getLocationName(lat, lon) {
-//   const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&zoom=18&format=jsonv2`;
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     console.log(response);
-//     console.log(data);
-//   } catch (error) {
-//     console.log("Failed to fetch");
-//   }
-// }
+async function getLocationName(lat, lon) {
+  const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&zoom=18&format=jsonv2`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(response);
+    console.log(data);
+  } catch (error) {
+    console.log("Failed to fetch");
+  }
+}
 
-// async function getWeather(lat, lon) {
-//   const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,sunrise,sunset,temperature_2m_max,temperature_2m_min&current=weather_code,is_day,temperature_2m,wind_speed_10m,relative_humidity_2m&timezone=auto`;
+async function getWeather(lat, lon) {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,sunrise,sunset,temperature_2m_max,temperature_2m_min&current=weather_code,is_day,temperature_2m,wind_speed_10m,relative_humidity_2m&timezone=auto`;
 
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   console.log(data);
-// }
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
 const now = new Date();
 const hour = now.getHours()
 const minute = now.getMinutes()
